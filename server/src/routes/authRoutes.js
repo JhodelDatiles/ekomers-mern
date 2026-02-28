bb4bc1de87dc1c6abf283df2dffb6859aff47ca1
JhodelDatiles <DatilesJhodel.io@gmail.com>
2026-02-28 13:40:20 +0800

    2802263

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, logout, getCurrentUser, refreshToken, verifyEmail, forgotPassword, resetPassword} from '../controllers/authController.js';
+import { register, login, logout, getCurrentUser, refreshToken, verifyEmail, forgotPassword, resetPassword, resendVerification} from '../controllers/authController.js';
 import { protect } from '../middlewares/protect.js'
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.post('/logout', logout);
 router.post('/refresh-token', refreshToken);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
+router.post('/resend-verification', resendVerification);
 router.get('/me', protect, getCurrentUser);
 router.get('/verify-email/:token', verifyEmail);
 
