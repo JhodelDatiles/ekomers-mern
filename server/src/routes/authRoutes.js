84399d6dd1c2929b1b126b0093c1cd26b8c89a71
JhodelDatiles <DatilesJhodel.io@gmail.com>
2026-07-22 14:19:28 +0800

    done fixing bugs on authentication ?

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -12,15 +12,16 @@ import {
 } from '../controllers/authController.js';
 import { forgotPasswordLimiter, authLimiter } from '../middlewares/rateLimiter.js';
 import { protect } from '../middlewares/protect.js'
+import { validatePassword } from "../middlewares/passwordValidation.js"
 
 const router = express.Router();
 
-router.post('/register',authLimiter, register);
+router.post('/register',authLimiter ,validatePassword('password'), register);
 router.post('/login',authLimiter, login);
 router.post('/logout', logout);
 router.post('/refresh-token', refreshToken);
 router.post('/forgot-password',forgotPasswordLimiter, forgotPassword);
-router.post('/reset-password',forgotPasswordLimiter, resetPassword);
+router.post('/reset-password',forgotPasswordLimiter ,validatePassword('newPassword'), resetPassword);
 router.post('/resend-verification',forgotPasswordLimiter, resendVerification);
 router.get('/me', protect, getCurrentUser);
 router.get('/verify-email/:token', verifyEmail);
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -8,13 +8,15 @@ import {
   verifyAccountDeletion // Make sure this matches the controller
 } from '../controllers/userController.js';
 import { protect } from '../middlewares/protect.js';
+import {forgotPasswordLimiter} from "../middlewares/rateLimiter.js"
+import {validatePassword} from "../middlewares/passwordValidation.js"
 
 const router = express.Router();
 
 router.get('/profile', protect, getProfile);
 router.put('/profile', protect, updateProfile);
-router.post('/request-code', protect, requestSecurityCode);
-router.post('/verify-password', protect, verifyPasswordChange);
-router.post('/verify-delete', protect, verifyAccountDeletion);
+router.post('/request-code',forgotPasswordLimiter , protect, requestSecurityCode);
+router.post('/verify-password',forgotPasswordLimiter ,validatePassword('new'), protect, verifyPasswordChange);
+router.post('/verify-delete',forgotPasswordLimiter , protect, verifyAccountDeletion);
 
 export default router;
